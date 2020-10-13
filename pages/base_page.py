263e93f4ffa288e715318953c0745de280a40834
unknown <C:\Program Files (x86)\The Bat!\Mail>
2020-10-13 12:46:02 +0800

    Add assert to MainPage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
 from selenium.webdriver import Remote as RemoteWebDriver
 
+#from selenium.common.exceptions import имя_исключения
+
+
 class BasePage():
     # Из комментов
     def __init__(self, browser: RemoteWebDriver, url, timeout=10):
@@ -7,6 +10,13 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicity_wait(timeout)
+    
+ #   def is_element_present(self, how, what):
+ #   try:
+ #       self.browser.find_element(how, what)
+ #   except (имя исключения):
+ #       return False
+ #   return True
     
     def open(self):
         self.browser.get(self.url)
