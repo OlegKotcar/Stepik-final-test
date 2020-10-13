6dbf9a42f6192f81a2f3a10a9d9b8f253fd30d5c
unknown <C:\Program Files (x86)\The Bat!\Mail>
2020-10-13 12:38:14 +0800

    Add should be login link

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,4 +8,4 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid") # селектор заведомо неправильный для теста
