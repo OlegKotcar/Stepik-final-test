b3eb895054af5d5550b8de81b5ffb7e6119e4371
unknown <C:\Program Files (x86)\The Bat!\Mail>
2020-10-13 12:41:20 +0800

    Add implicity to BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,10 +2,11 @@ from selenium.webdriver import Remote as RemoteWebDriver
 
 class BasePage():
     # Из комментов
-    def __init__(self, browser: RemoteWebDriver, url):
+    def __init__(self, browser: RemoteWebDriver, url, timeout=10):
         #def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+        self.browser.implicity_wait(timeout)
     
     def open(self):
         self.browser.get(self.url)
